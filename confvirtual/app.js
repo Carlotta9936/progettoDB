43689a858b3e804de507140249515576ee4abefa
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-03-29 16:54:02 +0200

    aggiunto signin

--- confvirtual/app.js
+++ confvirtual/app.js
@@ -3,24 +3,49 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+dotenv.config({ path: './.env' });
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'pug');    //Motore grafico
 
 app.use(logger('dev'));
+//Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: false}));
+//Parse JSON bodies (ad sent by API clients)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Importo per i moduli per indirizzamento
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+//Indirizzamento
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
+})
+
+db.connect((err, result) => {
+  if(err) {
+      console.log(err)
+  } else {
+      console.log("MySQL conneceted...")
+  }
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- confvirtual/routes/index.js
+++ confvirtual/routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+// Require controller modules
+const utenti = require('../controllers/utenti');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//SIGN IN
+router.get('/signin', (req, res) =>{
+  res.render('signin')
+})
+
+router.post('/signin', utenti.signin);          //Registra un nuovo utente
+
+/// USERS ROUTERS /// 
+router.get('/utenti/', utenti.users_page);      //Vedere tutti la pagina con tutti gli utenti
+router.get('/utenti/:id', utenti.user_page);    //Vedere la pagina di un utente
+
 
 module.exports = router;
