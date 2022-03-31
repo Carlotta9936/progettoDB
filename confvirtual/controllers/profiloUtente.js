93a1f5249a560305c2f3899388aa34801a6765f2
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-03-31 15:43:16 +0200

    informazioniPersonali

--- confvirtual/controllers/profiloUtente.js
+++ confvirtual/controllers/profiloUtente.js
@@ -6,13 +6,11 @@ const db = require('../connectionDB');
 var token;
 
 exports.informazioniPersonali = (req, res, next) => {
-    db.query(`SELECT username, nome, cognome, luogo_nascita, data_nascita FROM utente WHERE username = '${req.params.username}'`, (err, result) => {
+    db.query(`SELECT username, nome, cognome, luogo_nascita, data_nascita FROM utente WHERE username = 'Alsi'`, (err, result) => {
         if(err) { throw err; }
-        console.log(result);
-    })
-    
-
-    res.render('profile', {user: req.params.username, ruolo: "stronzo"});
+        console.log(result[0].nome);
+        res.render('profile-informazioniPersonali', {username: result[0].username, nome: result[0].nome, cognome: result[0].cognome, luogoNascita: result[0].luogo_nascita, dataNascita: result[0].data_nascita});
+    })    
 }
 
 exports.conferenze = (req, res, next) => {
