c77e1d91c39af7d951d283b57240ccf67825bb6a
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-03-31 14:34:50 +0200

    creato modulo connectionDB

--- confvirtual/app.js
+++ confvirtual/app.js
@@ -3,10 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mysql = require('mysql');
-const dotenv = require('dotenv');
-
-dotenv.config({ path: './.env' });
+const db = require('./connectionDB');
 
 var app = express();
 
@@ -34,16 +31,9 @@ app.use('/users', usersRouter);
 app.use('/conferenza',require('./routes/conferenza'));
 app.use('/sponsor',require('./routes/sponsor'));
 app.use('/sponsorizzazione',require('./routes/sponsorizzazione'));
+app.use('/profilo', require('./routes/profilo'));
 //app.use('/programma_giornaliero',require('./routes/programma_giornaliero'));
 
-
-
-const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE
-})
 
 //Connessione al database
 db.connect((err, result) => {
