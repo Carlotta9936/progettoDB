8df11df70894670fcb8fb6264956839dd52ebe79
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2022-04-02 15:03:45 +0200

    conferenza

--- confvirtual/routes/sponsor.js
+++ confvirtual/routes/sponsor.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+///     ROUTES PER LA CREAZIONE DI NUOVI SPONSOR
 const sponsorController = require ('../controllers/sponsor');
 
+//form per la creazione di un nuovo sponsor
 router.get('/nuovoSponsor', sponsorController.formSponsor);
 
 router.post('/nuovoSponsor', sponsorController.creaSponsor);
