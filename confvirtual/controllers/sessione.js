c5b4cf9a0e06d6750d7dd6c6a927d9f3b84eb42d
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-04-07 16:03:21 +0200

    sessione stored procedures

--- confvirtual/controllers/sessione.js
+++ confvirtual/controllers/sessione.js
@@ -3,20 +3,19 @@ const bcrypt = require('bcryptjs');
 const db = require('../connectionDB');
 
 exports.formSessione = (req, res)=>{
-    db.query('SELECT * FROM programma_giornaliero', function(err,result,fields){
+    db.query('call getProgrammaGiornaliero();', function(err,result,fields){
         if(err) throw err;
         else 
         //console.log(result);
-        res.render('newsessione', {programmi: result});
+        res.render('newsessione', {programmi: result[0]});
     });
 }
 
 exports.creaSessione = (req,res)=>{
     console.log(req.body);
     const {oraI, oraF, titolo, link, num, programma} = req.body;
-    //console.log(programma);
-
-    db.query(`INSERT INTO sessione(ora_f, ora_i, titolo, link, num_presentazioni, programma) VALUES ('${oraF}','${oraI}','${titolo}','${link}','${num}','${programma}');`,(err, results)=>{
+    
+    db.query(`call creaSessione ('${oraF}','${oraI}','${titolo}','${link}','${num}','${programma}');`,(err, results)=>{
         if(err){
             console.log(err);
         }else{
@@ -27,25 +26,16 @@ exports.creaSessione = (req,res)=>{
 
 exports.specificaSessione=(req,res)=>{
     //query che controlla la sessione abbia già delle presentazioni
-    let sqlverifica=`select *
-    from presentazione, sessione
-    where presentazione.sessione=sessione.id_sessione and sessione.id_sessione= "${req.params.id_sessione}"`;
-    db.query(sqlverifica,function(err,results){
+    db.query(`call presentazione ("${req.params.id_sessione}")`, function(err,results){
         if(err) throw err;
-        if (results.length==0){
+        if (results[0].length==0){
             console.log(req.params.titolo);
             res.render('sessioneVuota',{titolo: req.params.titolo});
         }else{
-            let sql=`(select articolo.titolo as titolo, presentazione.ora_f as oraf, presentazione.ora_i as orai, sessione.titolo as titolosessione
-            from articolo, sessione, presentazione
-            where presentazione.sessione=sessione.id_sessione and sessione.id_sessione= "${req.params.id_sessione}" and articolo.id_articolo=presentazione.id_presentazione)union
-            (select tutorial.titolo as titolo, presentazione.ora_f as oraf, presentazione.ora_i as orai, sessione.titolo as titolosessione
-                from tutorial, sessione, presentazione
-                where presentazione.sessione=sessione.id_sessione and sessione.id_sessione= "${req.params.id_sessione} and tutorial.id_tutorial=presentazione.id_presentazione")`;
-            db.query(sql,function(err,results){
+            db.query(`call articoloSessionePresentazione("${req.params.id_sessione}")`, function(err,results){
                 if(err) throw err;
-                console.log(results[0].oraf);
-                res.render('sessione',{presentazioni: results});
+                console.log(results[0][0].oraf);
+                res.render('sessione',{presentazioni: results[0]});
 
             });
         }
