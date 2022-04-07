51505f2ba389ee32296e26afd0193a6fa34647ad
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-04-07 15:18:52 +0200

    aggiustate date in profilo utente

--- confvirtual/controllers/profiloUtente.js
+++ confvirtual/controllers/profiloUtente.js
@@ -2,12 +2,14 @@ const mysql = require('mysql');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const db = require('../connectionDB');
+const { DateTime } = require('luxon');
 
 var token;
 
 exports.informazioniPersonali = (req, res, next) => {
     db.query(`call informazioniPersonali('${req.params.username}')`, (err, result) => {
         if(err) { throw err; }
+        result[0][0].data_nascita = DateTime.fromJSDate(result[0][0].data_nascita).toLocaleString(DateTime.DATE_MED);
         res.locals.informazioniPersonali = result[0][0];
         next();
     })
@@ -16,6 +18,10 @@ exports.informazioniPersonali = (req, res, next) => {
 exports.conferenze = (req, res, next) => {
     db.query(`call conferenze('${req.params.username}')`, (err, result) => {
         if(err) { throw err; }
+        for(var i=0; i<result[0].length; i++){
+            result[0][i].datainizio = DateTime.fromJSDate(result[0][i].datainizio).toLocaleString(DateTime.DATE_MED);
+            result[0][i].datafine = DateTime.fromJSDate(result[0][i].datafine).toLocaleString(DateTime.DATE_MED);
+        }
         res.locals.conferenze = result[0];
         next();
     })
@@ -24,6 +30,7 @@ exports.conferenze = (req, res, next) => {
 exports.presentazioniPreferite = (req, res, next) => {
     db.query(`call presentazioniPreferite('${req.params.username}')`, (err, result) => {
         if(err) { throw err; }
+        result[0][0].programma_giornalieroData = DateTime.fromJSDate(result[0][0].programma_giornalieroData).toLocaleString(DateTime.DATE_MED);
         res.locals.presentazioniPreferite = result[0];
         next();
     })
@@ -31,7 +38,7 @@ exports.presentazioniPreferite = (req, res, next) => {
 
 exports.renderizzaProfilo = (req, res) => {
     //console.log(res.locals);
-    console.log(res.locals.presentazioniPreferite);
+    //console.log(res.locals.presentazioniPreferite);
     res.render('profile', {
         username: res.locals.informazioniPersonali.username, 
         nome: res.locals.informazioniPersonali.nome, 
@@ -40,13 +47,5 @@ exports.renderizzaProfilo = (req, res) => {
         dataNascita: res.locals.informazioniPersonali.data_nascita,
         conferenze: res.locals.conferenze,
         presentazioni: res.locals.presentazioniPreferite
-        /*nomeConferenza: res.locals.conferenze.nome,
-        acronimoConferenza: res.locals.conferenze.acronimo,
-        annoConferenza: res.locals.conferenze.anno,
-        dataInizio: res.locals.conferenze.datainizio,
-        dataFine: res.locals.conferenze.datafine,
-        presentazioniNome: res.locals.presentazioniPreferite.conferenzaNome,
-        presentazioniData: res.locals.presentazioniPreferite.programma_giornalieroData*/
-    
     })
 }
