e96bb2e4e3974ed81f4e58022b34f4a39b6d93cd
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-04-11 17:37:09 +0200

    autenticazione

---
+++ query/stored procedures/autenticazione.sql
@@ -0,0 +1,8 @@
+DELIMITER $$
+CREATE PROCEDURE `autenticazione`(name varchar(30), password varchar(60))
+BEGIN
+	SELECT * FROM utente WHERE username = name AND password = password; 
+	SELECT ruolo FROM ruoli WHERE username = name;
+END$$
+
+DELIMITER ;
