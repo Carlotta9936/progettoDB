982831404c023048bfcdb36cd64d59cef00bd861
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2022-04-11 20:25:58 +0200

    sponsor max5

--- query/stored procedures/contasponsor.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER $$
-CREATE PROCEDURE `contasponsor`(in anno year(4),in acronimo varchar(10))
-BEGIN
-	select count(nome_sponsor)as num_sponsorizzazioni
-	from sponsorizzazione
-    where annoConf=anno and acronimoConf=acronimo
-	group by annoConf, acronimoConf;
-END$$
