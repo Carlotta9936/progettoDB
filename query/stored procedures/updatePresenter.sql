ea772fe2cc33402880db59826782ad53e89de624
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-04-13 13:46:38 +0200

    updatePresenter

--- query/stored procedures/updatePresenter.sql
+++ query/stored procedures/updatePresenter.sql
@@ -1,8 +1,8 @@
 DELIMITER $$
-CREATE PROCEDURE `updatePresenter`(username varchar(30), uni varchar(50), dip varchar(50))
+CREATE  PROCEDURE `updatePresenter`(username varchar(30), uni varchar(50), dip varchar(50), img varchar(50), curriculum varchar(50))
 BEGIN
-	INSERT INTO presenter (usernamePresenter, universita, dipartimento) VALUES (username, uni, dip);
-
+	INSERT INTO presenter (usernamePresenter, universita, dipartimento, foto, cv) VALUES (username, uni, dip, img, curriculum);
+    
 END$$
 
 DELIMITER ;
