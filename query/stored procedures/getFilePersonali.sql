1c4c89e9f98c039c638363d945ebabae1f6ea376
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-04-13 13:29:48 +0200

    getFilePersonale

---
+++ query/stored procedures/getFilePersonali.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+CREATE PROCEDURE `getFilePersonali`(username varchar(50))
+BEGIN
+	(select cv as cv, foto as image
+    from presenter
+    where usernamePresenter = username)
+    union
+    (select cv as cv, foto as image
+    from speaker
+    where usernameSpeaker = username);
+    
+END$$
+
+DELIMITER ;
