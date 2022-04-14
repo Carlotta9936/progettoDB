636c8473d5291d8e3246dfc70affb8101d334191
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-04-14 11:02:25 +0200

    caricamento file ora è un modulo richiamabile ovunque

--- confvirtual/modules/multer.js
+++ confvirtual/modules/multer.js
@@ -1,18 +1,15 @@
 const multer = require('multer');
+var path = require('path');
 var  upload  =  multer ( {  dest : 'public/uploads/'  } ) 
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '../public/img')},
-    filename:(req, file, cb) => {
-      cb(null, 'images/')
-      //return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-  })
-  
-  upload = multer ({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/')},
+  filename:(req, file, cb) => {
+    cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+  }
+})
+
+exports.upload = multer ({
     storage: storage
   })
-
-  
-  module.exports = storage;
