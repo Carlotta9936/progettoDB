71689d2c9f4b26a16c197eb9e5d5556d38070dd6
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-04-29 14:44:56 +0200

    bug1

--- query/stored procedures/nomesponsor.sql
+++ query/stored procedures/nomesponsor.sql
@@ -1,6 +1,10 @@
 DELIMITER $$
-CREATE  PROCEDURE `nomesponsor`()
+CREATE PROCEDURE `nomesponsor`(acronimo varchar(10), anno int)
 BEGIN
-	select sponsor.nome as nome
-	from sponsor;
+	SELECT nome
+	FROM confvirtual.sponsor 
+	WHERE nome not in (
+		SELECT nome_sponsor
+		FROM confvirtual.sponsorizzazione 
+		WHERE acronimoConf=acronimo and annoConf=anno);
 END$$
